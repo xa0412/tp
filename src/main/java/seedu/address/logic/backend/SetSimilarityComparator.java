0834750826347d4592ece7c503f0c4b5f04145f9
zuoshihua <zuoshihua@proton.me>
2025-04-02 18:24:54 +0800

    Compare set similarity instead

---
+++ src/main/java/seedu/address/logic/backend/SetSimilarityComparator.java
@@ -0,0 +1,26 @@
+package seedu.address.logic.backend;
+
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetSimilarityComparator<T> implements Comparator<Set<T>> {
+
+    private final Set<T> ref;
+
+    public SetSimilarityComparator(Set<T> ref) {
+        this.ref = ref;
+    }
+
+    @Override
+    public int compare(Set<T> o1, Set<T> o2) {
+        Set<T> r1 = new HashSet<>(ref);
+        Set<T> r2 = new HashSet<>(ref);
+
+        r1.retainAll(o1);
+        r2.retainAll(o2);
+
+        return Integer.compare(r2.size(), r1.size());
+    }
+
+}
